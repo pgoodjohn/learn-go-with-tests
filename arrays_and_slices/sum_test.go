e157e77c26fd362ca8e939a6177fe5aabde4a221
Pietro Bongiovanni <info@pietrobongiovanni.com>
2020-01-18 16:44:50 +0100

    Implemented test it adds an undefined range of numbers"

--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -15,6 +15,13 @@ func TestSum(t *testing.T) {
 	})
 
 	t.Run("it adds an undefined range of numbers", func(t *testing.T) {
-		t.Skip("To be implemented")
+		input := []int{1, 2, 3}
+		
+		actual := Sum(input)
+		expected := 6
+
+		if expected != actual {
+			t.Errorf("Expected: %d, actual: %d", expected, actual)
+		}
 	})
 }
