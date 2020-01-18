027a61de2def722fa671386ab38ebeb1622399c1
Pietro Bongiovanni <info@pietrobongiovanni.com>
2020-01-18 16:47:30 +0100

    Remove scenario it adds a fixed range of numbers

--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -3,16 +3,6 @@ package arrays
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Run("it adds a fixed range of numbers", func(t *testing.T) {
-		input := [5]int{1, 2, 3, 4, 5}
-
-		actual := Sum(input)
-		expected := 15
-
-		if expected != actual {
-			t.Errorf("Expected: %d, actual: %d", expected, actual)
-		}
-	})
 
 	t.Run("it adds an undefined range of numbers", func(t *testing.T) {
 		input := []int{1, 2, 3}
