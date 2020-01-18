421950f36ea462a281395da0bc2dff69f49e7d46
Pietro Bongiovanni <info@pietrobongiovanni.com>
2020-01-18 16:20:44 +0100

    Test letter is repeated 5 times

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -4,6 +4,11 @@ import "testing"
 
 func TestRepeat(t *testing.T) {
 	t.Run("letter is repeated 5 times", func(t *testing.T) {
-		t.Skip("To be implemented")
+		actual := Repeat("a")
+		expected := "aaaaa"
+
+		if expected != actual {
+			t.Errorf("Expected: %s, actual: %s", expected, actual)
+		}
 	})
 }
