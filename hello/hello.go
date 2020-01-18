49cca3f0ce2b92bbad5e5472250830f81a3a3843
Pietro Bongiovanni <info@pietrobongiovanni.com>
2020-01-18 10:18:13 +0100

    Add support for spanish language

--- hello/hello.go
+++ hello/hello.go
@@ -2,14 +2,21 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix =  "Hello, "
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
 // Hello is our domain, we want to separate it from the outside world
 func Hello(name string, language string) string {
+	var prefix string
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+	if language == "Spanish" {
+		prefix = spanishHelloPrefix
+	} else {
+		prefix = englishHelloPrefix
+	}
+	return prefix + name
 }
 
 func main() {
