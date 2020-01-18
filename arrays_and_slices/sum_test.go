d45e87904b874b6a35306d3f842f088d22d6c84d
Pietro Bongiovanni <info@pietrobongiovanni.com>
2020-01-18 16:35:30 +0100

    Implemented scenario it adds a range of numbers

--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -4,6 +4,13 @@ import "testing"
 
 func TestSum(t *testing.T) {
 	t.Run("it adds a range of numbers", func(t *testing.T) {
-		t.Skip("To be implemented")
+		input := [5]int{1, 2, 3, 4, 5}
+
+		actual := Sum(input)
+		expected := 15
+
+		if expected != actual {
+			t.Errorf("Expected: %d, actual: %d", expected, actual)
+		}
 	})
 }
