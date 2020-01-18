3b6c65548cbe667c8f79b511285edaf286b806c3
Pietro Bongiovanni <info@pietrobongiovanni.com>
2020-01-18 10:26:13 +0100

    Refactor hello()

--- hello/hello.go
+++ hello/hello.go
@@ -16,13 +16,17 @@ func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	if language == spanish {
-		return spanishHelloPrefix + name
-	} 
-	if language == french {
-		return frenchHelloPrefix + name
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix 
 	}
-	return englishHelloPrefix + name
+	return prefix + name
+
 }
 
 func main() {
