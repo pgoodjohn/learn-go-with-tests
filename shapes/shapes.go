5c3e452ba89543dff78763d71a9ed60586a0759b
Pietro Bongiovanni <info@pietrobongiovanni.com>
2020-01-19 17:04:22 +0100

    Introduce area calculation

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -4,3 +4,7 @@ func Perimeter(width float64, height float64) (perimeter float64) {
 	perimeter = 2 * (width + height)
 	return
 }
+
+func Area(width float64, height float64) (area float64) {
+	return
+}
