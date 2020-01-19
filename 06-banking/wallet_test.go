a93befc296f8971c0a6c65f53e1308800425fcfc
Pietro Bongiovanni <info@pietrobongiovanni.com>
2020-01-19 18:14:28 +0100

    Extract assert Wallet Balance

--- 06-banking/wallet_test.go
+++ 06-banking/wallet_test.go
@@ -3,29 +3,25 @@ package banking
 import "testing"
 
 func TestWallet(t *testing.T) {
+
+	assertWalletBalance := func(t *testing.T, w Wallet, expected Bitcoin) {
+		t.Helper()
+		if expected != w.Balance() {
+			t.Errorf("Expected: %s, actual: %s", expected, w.Balance()) 
+		}
+	}
+
 	t.Run("deposited value goes on balance", func(t *testing.T) {
 		wallet := Wallet{}
 
 		wallet.Deposit(Bitcoin(10))
 
-		actual := wallet.Balance()
-		expected := Bitcoin(10)
-
-		if expected != actual {
-			t.Errorf("Expected: %s, actual: %s", expected, actual)
-		}
+		assertWalletBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdrawn value is removed from balance", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-
 		wallet.Withdraw(Bitcoin(10))
-
-		expected := Bitcoin(10)
-		actual := wallet.Balance()
-
-		if expected != actual {
-			t.Errorf("Expected: %s, actual: %s", expected, actual) 
-		}
+		assertWalletBalance(t, wallet, Bitcoin(10))
 	})
 }
