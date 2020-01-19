f97695ed04bc74a12d5110cf6c99d4015f8936cc
Pietro Bongiovanni <info@pietrobongiovanni.com>
2020-01-19 17:52:16 +0100

    Refactor perimeter tests

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -4,28 +4,20 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 
-	assertShapePerimeter := func(t *testing.T, shape Shape, expected float64) {
-		t.Helper()
-		actual := shape.Perimeter()
-
-		if expected != actual {
-			t.Errorf("Expected: %.2f, actual: %.2f", expected, actual) 
-		}
+	perimeterTests := []struct {
+		shape Shape
+		expected float64
+	} {
+		{Rectangle{10, 10}, 40.0},
+		{Circle{10}, 31.41592653589793},
 	}
 
-	t.Run("perimeter of rectangle is 2b + 2h", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		expected := 40.0
-
-		assertShapePerimeter(t, rectangle, expected)
-	})
-
-	t.Run("perimeter of a circle is pi * r", func(t *testing.T) {
-		circle := Circle{10}
-		expected := 31.41592653589793
-
-		assertShapePerimeter(t, circle, expected)
-	})
+	for _, tt := range perimeterTests {
+		actual := tt.shape.Perimeter()
+		if tt.expected != actual {
+			t.Errorf("Expected: %g, actual: %g", tt.expected, actual) 
+		}
+	}
 }
 
 func TestArea(t *testing.T) {
