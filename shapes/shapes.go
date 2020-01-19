515a26e63aeca27deb86b2c1d722160c42f20a24
Pietro Bongiovanni <info@pietrobongiovanni.com>
2020-01-19 17:14:32 +0100

    Introduce circles

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -5,6 +5,10 @@ type Rectangle struct {
 	Height float64
 }
 
+type Circle struct {
+	Radius float64
+}
+
 func Perimeter(rectangle Rectangle) (perimeter float64) {
 	perimeter = 2 * (rectangle.Width + rectangle.Height)
 	return
