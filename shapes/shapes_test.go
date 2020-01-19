59edee3f71e284e902f51770565e54bc99f39428
Pietro Bongiovanni <info@pietrobongiovanni.com>
2020-01-19 17:02:00 +0100

    Introduce scenario area of rectangle is b x h

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -3,12 +3,18 @@ package shapes
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	t.Run("perimeter of rectangle is 2b x 2h", func(t *testing.T) {
+	t.Run("perimeter of rectangle is 2b + 2h", func(t *testing.T) {
 		actual := Perimeter(10.0, 10.0)
 		expected := 40.0
 
 		if expected != actual {
 			t.Errorf("Expected: %.2f, actual: %.2f", expected, actual)
 		}
+	})
+}
+
+func TestArea(t *testing.T) {
+	t.Run("area of rectangle is b x h", func(t *testing.T) {
+		t.Skip("To be implemented")
 	})
 }
