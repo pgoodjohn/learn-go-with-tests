5123c4b9a3d52de13609c24e1c8defe2383c927d
Pietro Bongiovanni <info@pietrobongiovanni.com>
2020-01-19 17:09:20 +0100

    Update method signatures

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -5,12 +5,12 @@ type Rectangle struct {
 	Height float64
 }
 
-func Perimeter(width float64, height float64) (perimeter float64) {
+func Perimeter(rectangle Rectangle) (perimeter float64) {
 	perimeter = 2 * (width + height)
 	return
 }
 
-func Area(width float64, height float64) (area float64) {
+func Area(rectangle Rectangle) (area float64) {
 	area = width * height
 	return
 }
