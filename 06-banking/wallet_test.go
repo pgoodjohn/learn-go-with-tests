fe9d868d0eebd547bbb851881676d889b714a75d
Pietro Bongiovanni <info@pietrobongiovanni.com>
2020-01-19 18:29:20 +0100

    Extract assertion functions

--- 06-banking/wallet_test.go
+++ 06-banking/wallet_test.go
@@ -2,25 +2,26 @@ package banking
 
 import "testing"
 
+func assertWalletBalance(t *testing.T, w Wallet, expected Bitcoin) {
+	t.Helper()
+	if expected != w.Balance() {
+		t.Errorf("Expected: %s, actual: %s", expected, w.Balance()) 
+	}
+}
+
+func assertError(t *testing.T, expected error, actual error) {
+	t.Helper()
+	if expected == nil {
+		t.Error("Expected error but got nil") 
+	}
+
+	if expected != actual {
+		t.Errorf("Expected: %q, actual: %q", expected, actual)
+	}
+}
+
+
 func TestWallet(t *testing.T) {
-
-	assertWalletBalance := func(t *testing.T, w Wallet, expected Bitcoin) {
-		t.Helper()
-		if expected != w.Balance() {
-			t.Errorf("Expected: %s, actual: %s", expected, w.Balance()) 
-		}
-	}
-
-	assertError := func(t *testing.T, expected error, actual error) {
-		t.Helper()
-		if expected == nil {
-			t.Error("Expected error but got nil") 
-		}
-
-		if expected != actual {
-			t.Errorf("Expected: %q, actual: %q", expected, actual)
-		}
-	}
 
 	t.Run("deposited value goes on balance", func(t *testing.T) {
 		wallet := Wallet{}
