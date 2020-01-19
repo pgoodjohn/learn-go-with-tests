ec490652b2a54fadf93c0a9273d6a153fbba43d0
Pietro Bongiovanni <info@pietrobongiovanni.com>
2020-01-19 18:06:15 +0100

    Test scenario balance is in Bitcoin

--- 06-banking/wallet_test.go
+++ 06-banking/wallet_test.go
@@ -6,10 +6,10 @@ func TestWallet(t *testing.T) {
 	t.Run("deposited value goes on balance", func(t *testing.T) {
 		wallet := Wallet{}
 
-		wallet.Deposit(10)
+		wallet.Deposit(Bitcoin(10))
 
 		actual := wallet.Balance()
-		expected := 10
+		expected := Bitcoin(10)
 
 		if expected != actual {
 			t.Errorf("Expected: %d, actual: %d", expected, actual)
