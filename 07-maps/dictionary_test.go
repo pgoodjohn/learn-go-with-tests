5e8bcf47f70b59377e230b9f940358a0d14867a6
Pietro Bongiovanni <info@pietrobongiovanni.com>
2020-01-27 20:15:17 +0100

    Test Dictionary type

--- 07-maps/dictionary_test.go
+++ 07-maps/dictionary_test.go
@@ -13,9 +13,9 @@ func assertStringsEqual(t *testing.T, expected string, actual string) {
 func TestSerach(t *testing.T) {
 
 	t.Run("test finds this is just a test", func(t *testing.T) {
-		dictionary := map[string]string{"test": "this is just a test"}
+		dictionary := Dictionary{"test": "this is just a test"}
 
-		actual := Search(dictionary, "test")
+		actual := dictionary.Search("test")
 		expected := "this is just a test"
 
 		assertStringsEqual(t, expected, actual)
