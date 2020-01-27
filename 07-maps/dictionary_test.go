62ac383587f39074cf32cac9958efd960314d417
Pietro Bongiovanni <info@pietrobongiovanni.com>
2020-01-27 20:14:32 +0100

    Refactor assertStringsEqual

--- 07-maps/dictionary_test.go
+++ 07-maps/dictionary_test.go
@@ -2,6 +2,14 @@ package dictionary
 
 import "testing"
 
+func assertStringsEqual(t *testing.T, expected string, actual string) {
+	t.Helper()
+
+	if expected != actual {
+		t.Errorf("Expected: %q, actual: %q", expected, actual) 
+	}
+}
+
 func TestSerach(t *testing.T) {
 
 	t.Run("test finds this is just a test", func(t *testing.T) {
@@ -10,9 +18,7 @@ func TestSerach(t *testing.T) {
 		actual := Search(dictionary, "test")
 		expected := "this is just a test"
 
-		if expected != actual {
-			t.Errorf("Expected: %q, actual: %q", expected, actual)
-		}
+		assertStringsEqual(t, expected, actual)
 	})
 
 }
