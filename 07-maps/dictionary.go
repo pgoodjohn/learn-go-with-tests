9c358edaa20583a2d8744ed4e02bca811248d3dc
Pietro Bongiovanni <info@pietrobongiovanni.com>
2020-02-03 19:06:40 +0100

    Prevent words from being overwritten

--- 07-maps/dictionary.go
+++ 07-maps/dictionary.go
@@ -5,6 +5,7 @@ import "errors"
 type Dictionary map[string]string
 
 var ErrNotFound = errors.New("could not find the word you were looking for")
+var ErrWordExists = errors.New("could not find the word you were looking for")
 
 func (d Dictionary) Search(word string) (string, error) {
 
@@ -16,6 +17,16 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	}
+
+	return nil
 }
