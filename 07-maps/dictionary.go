c44cad1469183b95e7db13f16d5df8ec2069479a
Pietro Bongiovanni <info@pietrobongiovanni.com>
2020-02-03 19:10:01 +0100

    Refactor error management

--- 07-maps/dictionary.go
+++ 07-maps/dictionary.go
@@ -1,11 +1,17 @@
 package dictionary
 
-import "errors"
-
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
-var ErrWordExists = errors.New("could not find the word you were looking for")
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("could not find the word you were looking for")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 
